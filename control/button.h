1783b935c5322c8c2154fdd41ee8e6812b867304
Jeffrey Condell <jdcrc@rit.edu>
2024-06-18 11:55:13 -0400

    added button.h header file

---
+++ control/button.h
@@ -0,0 +1,57 @@
+#ifndef BUTTON_H_
+#define BUTTON_H_
+
+# include	<X11/Xlib.h>
+# include	<X11/Xutil.h>
+# include	<X11/Xresource.h>
+# include	"control.h"
+# include	"../mille.h"
+
+struct button;
+
+Button CcreateButton (
+    char *text,
+    int	width,
+    GC gc,
+    XFontStruct	*font,
+    long backcolor,
+    int	borderwidth
+);
+
+void buttonText (
+    Window w,
+    struct button *b,
+    GC gc
+);
+
+void buttonOn (
+    Window w,
+    struct button *b
+);
+
+void buttonOff (
+    Window w,
+    struct button *b
+);
+
+Window CmapButton (
+    Window	parent,
+    int	x,
+    int y,
+    Button	button,
+    int	(*notify)()
+);
+
+int CunmapButton (
+    Window w
+);
+
+int CmanageButton (
+    XAnyEvent *rep
+);
+
+int CredrawButton (
+    Window w;
+);
+
+#endif
